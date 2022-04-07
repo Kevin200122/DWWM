9e618be8cb3c04240d8a5133403afcb0ca4b61d9
27010-34-02 <27010-34-02@exchange.ad.afpanet>
2022-04-07 16:25:16 +0200

    javascript

---
+++ Javascript/script/Exercice1.js
@@ -0,0 +1,17 @@
+let NomSaisie;
+let PrenomSaisie;
+
+
+function Saisie(Nom, Prenom){
+  
+    let NomPrenom;
+    
+
+    NomPrenom=Nom + " " + Prenom;
+    return NomPrenom;
+
+}
+
+NomSaisie=prompt("Donnez votre nom:");
+PrenomSaisie=prompt("Donnez votre prénom:");
+alert("Hello " + Saisie(NomSaisie,PrenomSaisie));

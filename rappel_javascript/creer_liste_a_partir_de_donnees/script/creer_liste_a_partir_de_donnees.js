dc50088c19f9906a1bad3c4e2ea0b12268302aee
LEFEBVRE Kevin <lefebvrekevin2011@gmail.com>
2022-05-31 21:17:08 +0200

    cours_javascript

--- rappel_javascript/creer_liste_a_partir_de_donnees/script/creer_liste_a_partir_de_donnees.js
+++ rappel_javascript/creer_liste_a_partir_de_donnees/script/creer_liste_a_partir_de_donnees.js
@@ -2,17 +2,31 @@ const liste = document.querySelector('.liste');
 const btn = document.querySelector('btn');
 const xhr = new XMLHttpRequest();
 
-btn.addEventListener('click', () =>{
+btn.addEventListener('click', () => {
     console.log("Bonjour tout le monde!");
 })
 
-xhr.onload('Get', 'https://jsonplaceholder.typicode.com/posts');
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
 xhr.responseType = 'json';
 
-xhr.onload = function() {
+xhr.onload = function () {
     //console.log(JSON.parse(xhr.response));
     console.log(xhr.response);
+
+
+    for (i = 0; i < xhr.response.length; i++) {
+        let newLi = document.createElement('li');
+        let newTitreCarte = document.createElement('h2');
+        let newBodyCarte = document.createElement('p');
+
+        newTitreCarte.innerText = xhr.response[i].title;
+        newBodyCarte.innerText = xhr.response[i].body;
+
+        newLi.appendChild(newTitreCarte);
+        newLi.appendChild(newBodyCarte);
+        liste.appendChild(newLi);
+    }
 }
 
 xhr.send();

c49e2ae5531b56b1738e69d14e01b5bb81c8171c
LEFEBVRE Kevin <lefebvrekevin2011@gmail.com>
2022-06-10 12:01:21 +0200

    exercice_discord

--- Exercice_javascript_discord/exercicef11/script/definir_nombre_a_deviner.js
+++ Exercice_javascript_discord/exercicef11/script/definir_nombre_a_deviner.js
@@ -1,28 +1,43 @@
-var choixOrdinateur = Math.floor((Math.random() * 100) +1),
-choixPersonne;
+// var choixOrdinateur = Math.floor((Math.random() * 100) + 1),
+//     choixPersonne;
+
+// console.log(choixOrdinateur);
+
+// var finJeu = true;
+// choixPersonne = prompt("Tu dois choisir un nombre entre 1 et 100, fais ton choix!");
+
+// if (choixPersonne < choixOrdinateur) {
+//     alert("Le nombre est plus grand, recommence!");
+//     var choixPersonne = prompt("Tu dois choisir un nombre entre 1 et 100, fais ton choix!");
+// } else if (choixPersonne > choixOrdinateur) {
+//     alert("Le nombre est plus petit, recommence!");
+//     var choixPersonne = prompt("Tu dois choisir un nombre entre 1 et 100, fais ton choix!");
+// }
+
+// alert("Mes félicitations tu as gagné, tu as trouvé le nombre.");
+
+
+var choixOrdinateur = Math.floor((Math.random() * 100) + 1),//Choix d'un chiffre entre 0 et 100
+    choixPersonne;
 
 console.log(choixOrdinateur);
 
-var finJeu = true;
+var fin = true;
+choixPersonne = prompt("Tu dois choisir un nombre entre 0 et 100, va-y fais ton choix!");
 
-while(finJeu) {
-    if(choixOrdinateur === choixPersonne) {
-        alert("Vous avez deviner le bon chiffre, félicitation!");
-        var finJeu = false;
-    }
-    else {
-        if (choixPersonne < choixOrdinateur) {
-            alert("Le nombre est plus grand!");
-            var choixPersonne = prompt("Choix de votre chiffre entre 0 et 100");
-        }
-    else {
-            alert("Le nombre doit être plus petit!");
-            var choixPersonne = prompt("choix de votre chiffre entre 0 et 100");
-        }
-    
-    }
-     
+// Pas besoin d'un booléen pour ce cas la, on met la condition directement dans le while
+while (choixOrdinateur != choixPersonne) {
+
+    // Si il n'y a qu'une seule instruction, il n' y a pas besoin des accolades {}
+
+    if (choixPersonne < choixOrdinateur)
+        alert(" le nombre doit être plus grand, recommence!");
+    else
+        alert("Le nombre doit être plus petit recommence!");
+
+    choixPersonne = prompt("Tu dois choisir un nombre entre 0 et 100, fais ton choix!");
 }
 
+// Ce message ne s'affichera seulement si le choixPersonne == choixOrdinateur (boucle while(tant que) vérifiée)
+alert("vous avez trouvé le bon chiffre bravo !");
 
-       
---
+++ Exercice_javascript_discord/exercicef12/script/devinez_trois_essaies.js
@@ -0,0 +1,17 @@
+// var choixOrdinateur = Math.floor((Math.random() * 100) + 1),
+//     choixPersonne;
+
+// console.log(choixOrdinateur);
+
+// var finJeu = true;
+// choixPersonne = prompt("Tu dois choisir un nombre entre 1 et 100, fais ton choix!");
+
+// if (choixPersonne < choixOrdinateur) {
+//     alert("Le nombre est plus grand, recommence!");
+//     var choixPersonne = prompt("Tu dois choisir un nombre entre 1 et 100, fais ton choix!");
+// } else if (choixPersonne > choixOrdinateur) {
+//     alert("Le nombre est plus petit, recommence!");
+//     var choixPersonne = prompt("Tu dois choisir un nombre entre 1 et 100, fais ton choix!");
+// }
+
+// alert("Mes félicitations tu as gagné, tu as trouvé le nombre.");

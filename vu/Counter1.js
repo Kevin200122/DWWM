1f4fdf4cd33b49d7d434930815c2b7f0db009512
LEFEBVRE Kevin <lefebvrekevin2011@gmail.com>
2023-04-20 17:18:55 +0200

    Vu

---
+++ vu/Counter1.js
@@ -0,0 +1,27 @@
+const Counter = {
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    template: "{{time()}} &nbsp;&nbsp; Le compteur vaut : {{count}}",
+    created() {
+        setInterval(() => {
+            this.count += 1;
+        }, 1000);
+    },
+    methods: {
+        time() {
+            //retourner l'heure sous la forme HH:MM:SS
+            var date = new Date();
+            var hour = date.getHours();
+            var min = date.getMinutes();
+            var sec = date.getSeconds();
+            if (hour < 10) hour = "0" + hour;
+            if (min < 10) min = "0" + min;
+            if (sec < 10) sec = "0" + sec;
+            return "" + hour + ":" + min + ":" + sec + " ";
+        },
+    },
+};
+export default Counter;
